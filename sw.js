e93232468ac9ac6782975007495220686c725a6a
elodie24000 <plassin.elodie24@gmail.com>
2026-06-17 12:03:18 +0200

    PWA - application installable sur telephone

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'cidil-v1';
+const ASSETS = [
+  '/logisitique-CIDIL/',
+  '/logisitique-CIDIL/index.html',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500&family=DM+Mono:wght@400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
